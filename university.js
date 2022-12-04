f14bed5b069727daaa95cd956941ec185de9e2b2
TigerX <100389933+TigerX1112@users.noreply.github.com>
2022-12-03 19:23:58 -0500

    Add files via upload

--- university.js
+++ university.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
-    //$("universityList").html("");
-    $.get("http://universities.hipolabs.com/search?country=Canada", function(data, status) {
-        if (status == "success" && data != null && data != undefined) {
-            for (let index = 0; index < data.length; index++) {
-                $("#universityList").append(`<a target="_blank" href="${data[index].web_pages[0]}" class="list-group-item list-group-item-action">${data[index].name}</a>`);
-
-            }
-        } else {
-            $("#universityList").append("Can't load the list from url.");
-        }
+    $( ".target" ).change(function () {
+        $( "select option:selected" ).each(function() {
+            country = $(this).val();
+            $("#test1").html("Universitys in "+country);
+        });
+        var url = "http://universities.hipolabs.com/search?country="+country+"";
+        $("test1").click(function() {
+            $.get(url, function(data, status) {
+                $("universityList").html("");
+                    if (status == "success" && data != null && data != undefined) {
+                        for (let index = 0; index < data.length; index++) {
+                            $("#universityList").append(`<a target="_blank" href="${data[index].web_pages[0]}" class="list-group-item list-group-item-action">${data[index].name}</a>`);
+                        }
+                    }   else {
+                        $("#universityList").append("Can't load data");
+                    }
+                });
+            });
     });
 });
