7c2199da870ae03969fb882bc04519eb7682f845
TigerX <100389933+TigerX1112@users.noreply.github.com>
2022-12-11 20:21:09 -0500

    Add files via upload

--- university.js
+++ university.js
@@ -1,21 +1,44 @@
 $(document).ready(function() {
-    $( ".target" ).change(function () {
-        $( "select option:selected" ).each(function() {
-            country = $(this).val();
-            $("#test1").html("Universitys in "+country);
-        });
-        var url = "http://universities.hipolabs.com/search?country="+country+"";
-        $("test1").click(function() {
-            $.get(url, function(data, status) {
-                $("universityList").html("");
-                    if (status == "success" && data != null && data != undefined) {
-                        for (let index = 0; index < data.length; index++) {
-                            $("#universityList").append(`<a target="_blank" href="${data[index].web_pages[0]}" class="list-group-item list-group-item-action">${data[index].name}</a>`);
-                        }
-                    }   else {
-                        $("#universityList").append("Can't load data");
-                    }
-                });
-            });
+    $.get('countries.json', function(data, status){
+        more_countrys = data;
     });
+
+    setButtonText();
+    generateUniversitiesList(getSelectedValue());
+
+    $("#target").change(function() {
+        setButtonText();
+        generateUniversitiesList(getSelectedValue());
+    });
+
+    function generateUniversitiesList(country) {
+        var urlLink = "http://universities.hipolabs.com/search?country=" + country;
+        $.get(urlLink, function(data, status) {
+            if (status == "success" && data != null && data != undefined) {
+                $("#universityList").html('');
+                for (let index = 0; index < data.length; index++) {
+                    $("#universityList").append(`<a target="_blank" href="${data[index].web_pages[0]}" class="list-group-item list-group-item-action">${data[index].name}</a>`);
+
+                }
+            } else {
+                $("#universityList").append("Can't load the list from url.");
+            }
+        });
+    }
+    function getSelectedValue() {
+        return $('#target').find(":selected").val();
+    }
+
+    function setButtonText() {
+        $("#collapseButton").html("Universities in " + $('#target').find(":selected").text());
+    }
+    
+    function addVal() {
+        optionText = more_countrys[Math.random().name];
+        optionValue = more_countrys[Math.random().name];
+  
+            $('.test').append(`<option value="${optionValue}">
+                                       ${optionText}
+                                       </option>`);
+    }
 });
